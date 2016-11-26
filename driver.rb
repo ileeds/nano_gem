f4af749f1d04cb7d7549dd506e4eaaac113f3675
Ian Leeds <ileeds@brandeis.edu>
2016-11-26 09:58:43 -0500

    service

---
+++ driver.rb
@@ -0,0 +1,7 @@
+require_relative 'nanoTwitter.rb'
+
+a=NanotwitterClient.new("client")
+puts a.find_user(1)
+puts a.find_tweet(1)
+puts a.recent_tweets(20)
+puts a.users_recent_tweets(1,20)
---
+++ nanoTwitter.rb
@@ -0,0 +1,52 @@
+require 'typhoeus'
+require 'byebug'
+require 'json'
+
+class NanotwitterClient
+  def initialize (name)
+    @name = name
+  end
+
+  def find_tweet(id)
+    start("/tweets/#{id}")
+  end
+
+  def find_user(id)
+    start("/users/#{id}")
+  end
+
+	#tweets is 'how many recent tweets you want'
+  def recent_tweets(count)
+    start("/tweets/recent?#{count}")
+  end
+
+  def users_recent_tweets(id, count)
+    start("/users/#{id}/tweets?#{count}")
+  end
+
+  def start (url)
+    request = Typhoeus::Request.new(
+      "https://cryptic-mountain-56365.herokuapp.com/api/v1"+url,
+      method: :get,
+			# "http://localhost:4567/api/v1"+url,
+      # method: :get,
+    )
+
+    request.on_complete do |response|
+      if response.success?
+        puts "success!"
+        return response.body
+      elsif response.timed_out?
+        puts "sorry, you timed out"
+      elsif response.code == 0
+        puts response.return_message
+      else
+        puts "HTTP request failed: " + response.code.to_s
+      end
+    end
+
+    request.run
+
+  end
+
+end
